6520ee4a99f8a3ef3ce9341ee0f09e90685434b3
lyokato <lyo.kato@gmail.com>
2016-05-01 22:10:11 +0900

    adds test for 'password' grant_type

---
+++ token_endpoint_password_test.go
@@ -0,0 +1,150 @@
+package goidc
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/lyokato/goidc/basic_auth"
+	"github.com/lyokato/goidc/grant"
+	th "github.com/lyokato/goidc/test_helper"
+)
+
+func TestTokenEndpointPassword(t *testing.T) {
+	te := NewTokenEndpoint()
+	te.Support(grant.Password())
+
+	sdi := th.NewTestStore()
+	user := sdi.CreateNewUser("user01", "pass01")
+	client := sdi.CreateNewClient(user.Id, "client_id_01", "client_secret_01", "http://example.org/callback")
+	client.AllowToUseGrantType(grant.TypePassword)
+
+	ts := httptest.NewServer(te.Handler(sdi))
+	defer ts.Close()
+
+	th.TokenEndpointSuccessTest(t, ts,
+		map[string]string{
+			"grant_type": "password",
+			"scope":      "offline_access",
+			"username":   "user01",
+			"password":   "pass01",
+		},
+		map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded; charset=UTF-8",
+			"Authorization": basic_auth.Header("client_id_01", "client_secret_01"),
+		},
+		200,
+		map[string]th.Matcher{
+			"Content-Type":  th.NewStrMatcher("application/json; charset=UTF-8"),
+			"Pragma":        th.NewStrMatcher("no-cache"),
+			"Cache-Control": th.NewStrMatcher("no-store"),
+		},
+		map[string]th.Matcher{
+			"access_token":  th.NewStrMatcher("ACCESS_TOKEN_0"),
+			"refresh_token": th.NewStrMatcher("REFRESH_TOKEN_0"),
+			"expires_in":    th.NewInt64Matcher(60 * 60 * 24),
+		},
+		nil)
+
+	th.TokenEndpointSuccessTest(t, ts,
+		map[string]string{
+			"grant_type": "password",
+			"username":   "user01",
+			"password":   "pass01",
+		},
+		map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded; charset=UTF-8",
+			"Authorization": basic_auth.Header("client_id_01", "client_secret_01"),
+		},
+		200,
+		map[string]th.Matcher{
+			"Content-Type":  th.NewStrMatcher("application/json; charset=UTF-8"),
+			"Pragma":        th.NewStrMatcher("no-cache"),
+			"Cache-Control": th.NewStrMatcher("no-store"),
+		},
+		map[string]th.Matcher{
+			"access_token":  th.NewStrMatcher("ACCESS_TOKEN_0"),
+			"refresh_token": th.NewAbsentMatcher(),
+			"expires_in":    th.NewInt64Matcher(60 * 60 * 24),
+		},
+		nil)
+
+}
+
+func TestTokenEndpointPasswordInvalidRequest(t *testing.T) {
+	te := NewTokenEndpoint()
+	te.Support(grant.Password())
+
+	sdi := th.NewTestStore()
+	user := sdi.CreateNewUser("user01", "pass01")
+	client := sdi.CreateNewClient(user.Id, "client_id_01", "client_secret_01", "http://example.org/callback")
+	client.AllowToUseGrantType(grant.TypePassword)
+
+	ts := httptest.NewServer(te.Handler(sdi))
+	defer ts.Close()
+
+	// missing username
+	th.TokenEndpointErrorTest(t, ts,
+		map[string]string{
+			"grant_type": "password",
+			"scope":      "offline_access",
+			"password":   "pass01",
+		},
+		map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded; charset=UTF-8",
+			"Authorization": basic_auth.Header("client_id_01", "client_secret_01"),
+		},
+		400,
+		map[string]th.Matcher{
+			"Content-Type":  th.NewStrMatcher("application/json; charset=UTF-8"),
+			"Pragma":        th.NewStrMatcher("no-cache"),
+			"Cache-Control": th.NewStrMatcher("no-store"),
+		},
+		map[string]th.Matcher{
+			"error":             th.NewStrMatcher("invalid_request"),
+			"error_description": th.NewStrMatcher("missing 'username' parameter"),
+		})
+
+	// missing password
+	th.TokenEndpointErrorTest(t, ts,
+		map[string]string{
+			"grant_type": "password",
+			"scope":      "offline_access",
+			"username":   "user01",
+		},
+		map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded; charset=UTF-8",
+			"Authorization": basic_auth.Header("client_id_01", "client_secret_01"),
+		},
+		400,
+		map[string]th.Matcher{
+			"Content-Type":  th.NewStrMatcher("application/json; charset=UTF-8"),
+			"Pragma":        th.NewStrMatcher("no-cache"),
+			"Cache-Control": th.NewStrMatcher("no-store"),
+		},
+		map[string]th.Matcher{
+			"error":             th.NewStrMatcher("invalid_request"),
+			"error_description": th.NewStrMatcher("missing 'password' parameter"),
+		})
+
+	// invalid user credential
+	th.TokenEndpointErrorTest(t, ts,
+		map[string]string{
+			"grant_type": "password",
+			"scope":      "offline_access",
+			"username":   "invalid_user",
+			"password":   "pass01",
+		},
+		map[string]string{
+			"Content-Type":  "application/x-www-form-urlencoded; charset=UTF-8",
+			"Authorization": basic_auth.Header("client_id_01", "client_secret_01"),
+		},
+		400,
+		map[string]th.Matcher{
+			"Content-Type":  th.NewStrMatcher("application/json; charset=UTF-8"),
+			"Pragma":        th.NewStrMatcher("no-cache"),
+			"Cache-Control": th.NewStrMatcher("no-store"),
+		},
+		map[string]th.Matcher{
+			"error": th.NewStrMatcher("invalid_grant"),
+		})
+}
