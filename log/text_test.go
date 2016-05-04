faa86d40989473034f11cf085c5cf387093279f5
lyokato <lyo.kato@gmail.com>
2016-05-04 11:33:03 +0900

    fotgot colored log test

--- log/text_test.go
+++ log/text_test.go
@@ -6,7 +6,7 @@ func TestTokenEndpointLog(t *testing.T) {
 	actual := TokenEndpointLog("authorization_code", InterfaceUnsupported, map[string]string{
 		"method": "FoobarMethod",
 	}, "foobar")
-	expected := "[goidc.TokenEndpoint:authorization_code] <InterfaceUnsupported method=\"FoobarMethod\">: foobar"
+	expected := "[goidc.TokenEndpoint:\x1b[35mauthorization_code\x1b[m] <\x1b[33mInterfaceUnsupported\x1b[m method=\"FoobarMethod\">: foobar"
 	if actual != expected {
 		t.Errorf("Log:\n - got: %v\n - want: %v\n", actual, expected)
 	}
